23ba196f97cc9eeb142af5d950c30a240a79fb80
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-29 16:07:58 +0900

    feat: Header, Footer 컴포넌트 구현
    Header 및 Footer 컴포넌트를 추가하고 필요한 기능과 스타일을 구현

---
+++ src/components/static/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="text-muted py-5">
+    <b-container>
+      <footer class="py-3 my-4">
+        <b-nav class="justify-content-center border-bottom pb-3 mb-3">
+          <b-nav-item href="#!" class="nav-item px-3">공지사항</b-nav-item>
+          <b-nav-item href="#!" class="nav-item px-3">커뮤니티</b-nav-item>
+          <b-nav-item href="#!" class="nav-item px-3">사진</b-nav-item>
+          <b-nav-item href="#!" class="nav-item px-3">문의</b-nav-item>
+        </b-nav>
+        <p class="text-center text-muted">&copy; 2023 NOW, Inc</p>
+      </footer>
+    </b-container>
+  </footer>
+</template>
---
+++ src/components/static/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <b-navbar fluid="lg" class="px-3 px-lg-5 navbar navbar-expand-lg navbar-light bg-light text-black">
+      <b-navbar-brand to="/">NOW</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+
+        <b-navbar-nav class="me-auto mb-2 mb-lg-0 ms-lg-4 text-truncate">
+          <b-nav-item href="#!" class="nav-item px-3">공지사항</b-nav-item>
+          <b-nav-item href="#!" class="nav-item px-3">커뮤니티</b-nav-item>
+          <b-nav-item href="#!" class="nav-item px-3">사진</b-nav-item>
+          <b-nav-item href="#!" class="nav-item px-3">문의</b-nav-item>
+        </b-navbar-nav>
+
+        <b-navbar-form class="d-flex px-3">
+          <b-form-input type="text" placeholder="게시글 검색" class="me-2"></b-form-input>
+          <b-button variant="primary" class="text-nowrap" type="button">검색</b-button>
+        </b-navbar-form>
+
+        <b-navbar-nav style="font-size: 1.5rem;">
+          <b-nav-item href="#" class="ps-3">
+            <i class="bi bi-chat-left-dots"></i>
+          </b-nav-item>
+          <b-nav-item href="#" class="ps-3">
+            <i class="bi bi-person"></i>
+          </b-nav-item>
+        </b-navbar-nav>
+
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
