df9838af8a0e7f37b5e87b40a27c16940bcf3319
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-30 12:11:48 +0900

    feat: router 업데이트

--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,18 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import Index from "@/pages/Index.vue";
 import Community from "@/pages/Community.vue";
+import Post from "@/pages/CommunityPost.vue";
+import Photos from "@/pages/Photos.vue";
+import Photo from "@/pages/Photo.vue";
+import PostForm from "@/pages/CommunityForm.vue";
 
 const routes = [
     {path: '/', name: 'Index', component: Index},
-    {path: '/Community', name: 'Community', component: Community},
+    {path: '/community', name: 'Community', component: Community},
+    {path: '/community/post', name: 'Post', component: Post},
+    {path: '/photo', name: 'Photo', component: Photo},
+    {path: '/photos', name: 'Photos', component: Photos},
+    {path: '/community/form', name: 'PostForm', component: PostForm},
 ]
 
 const router = createRouter({
