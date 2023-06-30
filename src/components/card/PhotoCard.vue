61b85c9fb5f23d7dc953df2174b4c87b199d2608
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-30 12:10:53 +0900

    feat: Photos 컴포넌트 디자인 구현

---
+++ src/components/card/PhotoCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-card class="p-2">
+    <div class="text-right"><small>Full Time</small></div>
+    <div class="text-center mt-2 p-3">
+      <b-img src="https://img.icons8.com/color/96/000000/google-logo.png" width="60"></b-img>
+      <span class="d-block font-weight-bold">UX Designer</span>
+      <hr>
+      <span>Google Inc</span>
+      <div class="d-flex flex-row align-items-center justify-content-center">
+        <i class="fa fa-map-marker"></i>
+        <small class="ml-1">FA 100, OH, USA</small>
+      </div>
+      <div class="d-flex justify-content-between mt-3">
+        <span>$40,000</span>
+        <b-button variant="outline-dark" size="sm">Apply Now</b-button>
+      </div>
+    </div>
+  </b-card>
+</template>
---
+++ src/pages/Photos.vue
@@ -0,0 +1,24 @@
+<script setup>
+import PhotoCard from "@/components/card/PhotoCard.vue";
+import SearchForm from "@/components/common/SearchForm.vue";
+import Category from "@/components/common/Category.vue";
+import BackgroundBanner from "@/components/common/BackgroundBanner.vue";
+</script>
+<template>
+  <BackgroundBanner :title="`Photo`" :bannerPath="`community.png`"/>
+
+  <b-container class="mt-3">
+    <b-row>
+      <b-col class="3">
+        <searchForm/>
+        <Category/>
+        <b-row>
+          <b-col class="mt-5 mb-3" cols="3" v-for="idx in 8" :key="idx">
+            <PhotoCard/>
+          </b-col>
+        </b-row>
+      </b-col>
+    </b-row>
+
+  </b-container>
+</template>
