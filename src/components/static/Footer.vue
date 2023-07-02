e337e96bfcbf6eaec8a0dd49c5f76c569e9902cd
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-07-02 18:24:02 +0900

    feat: Header, Footer 컴포넌트에서 router 이동을 위한 router-link 태그 추가

--- src/components/static/Footer.vue
+++ src/components/static/Footer.vue
@@ -3,10 +3,18 @@
     <b-container>
       <footer class="py-3 my-4">
         <b-nav class="justify-content-center border-bottom pb-3 mb-3">
-          <b-nav-item href="#!" class="nav-item px-3">공지사항</b-nav-item>
-          <b-nav-item href="#!" class="nav-item px-3">커뮤니티</b-nav-item>
-          <b-nav-item href="#!" class="nav-item px-3">사진</b-nav-item>
-          <b-nav-item href="#!" class="nav-item px-3">문의</b-nav-item>
+          <b-nav-item class="nav-item px-3">
+            <router-link :to="{ name: 'Notices' }" class="router-link text-decoration-none text-secondary">공지사항</router-link>
+          </b-nav-item>
+          <b-nav-item class="nav-item px-3">
+            <router-link :to="{ name: 'Community' }" class="router-link text-decoration-none text-secondary">커뮤니티</router-link>
+          </b-nav-item>
+          <b-nav-item class="nav-item px-3">
+            <router-link :to="{ name: 'Photos' }" class="router-link text-decoration-none text-secondary">사진</router-link>
+          </b-nav-item>
+          <b-nav-item class="nav-item px-3">
+            <router-link :to="{ name: 'Inquiries' }" class="router-link text-decoration-none text-secondary">문의</router-link>
+          </b-nav-item>
         </b-nav>
         <p class="text-center text-muted">&copy; 2023 NOW, Inc</p>
       </footer>
--- src/components/static/Header.vue
+++ src/components/static/Header.vue
@@ -8,10 +8,19 @@
       <b-collapse id="nav-collapse" is-nav>
 
         <b-navbar-nav class="me-auto mb-2 mb-lg-0 ms-lg-4 text-truncate">
-          <b-nav-item href="#!" class="nav-item px-3">공지사항</b-nav-item>
-          <b-nav-item href="#!" class="nav-item px-3">커뮤니티</b-nav-item>
-          <b-nav-item href="#!" class="nav-item px-3">사진</b-nav-item>
-          <b-nav-item href="#!" class="nav-item px-3">문의</b-nav-item>
+
+          <b-nav-item class="nav-item px-3">
+            <router-link :to="{ name: 'Notices' }" class="router-link text-decoration-none text-secondary">공지사항</router-link>
+          </b-nav-item>
+          <b-nav-item class="nav-item px-3">
+            <router-link :to="{ name: 'Community' }" class="router-link text-decoration-none text-secondary">커뮤니티</router-link>
+          </b-nav-item>
+          <b-nav-item class="nav-item px-3">
+            <router-link :to="{ name: 'Photos' }" class="router-link text-decoration-none text-secondary">사진</router-link>
+          </b-nav-item>
+          <b-nav-item class="nav-item px-3">
+            <router-link :to="{ name: 'Inquiries' }" class="router-link text-decoration-none text-secondary">문의</router-link>
+          </b-nav-item>
         </b-navbar-nav>
 
         <b-navbar-form class="d-flex px-3">
