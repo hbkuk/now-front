6367908a9a223a9f588d145d6c3c8e2f382e0571
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-08-12 05:14:29 +0900

    fix: devServer 객체 중첩 사용으로 인한 수정

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,21 @@
-const {defineConfig} = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
     transpileDependencies: true,
     lintOnSave: false,
     devServer: {
-        devServer: {
-            allowedHosts: "all",
-            client: {
-                webSocketURL: 'auto://0.0.0.0:0/ws'
+        allowedHosts: "all",
+        client: {
+            webSocketURL: 'auto://0.0.0.0:0/ws'
+        },
+        proxy: {
+            '/api': {
+                target: 'http://52.79.152.234:4040',
+                changeOrigin: true,
             },
-            proxy: {
-                '/api': {
-                    target: 'http://52.79.152.234:4040',
-                    changeOrigin: true,
-                },
-                '/attachments': {
-                    target: 'http://52.79.152.234:4040',
-                    changeOrigin: true,
-                }
+            '/attachments': {
+                target: 'http://52.79.152.234:4040',
+                changeOrigin: true,
             }
         }
     }
